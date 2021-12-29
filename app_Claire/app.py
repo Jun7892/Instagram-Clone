3f14b92565371622e085f4b2f433f6f57ed22f87
Claire Chung <35644336+haewonchung@users.noreply.github.com>
2021-12-30 03:43:17 +0900

    Added js effects

--- app_Claire/app.py
+++ app_Claire/app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 # from werkzeug.utils import secure_filename
 import datetime
 import certifi
+import gridfs
 ca = certifi.where()
 app = Flask(__name__)
 client = MongoClient('mongodb+srv://test:sparta@cluster0.bep7j.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
@@ -75,6 +76,25 @@ def index():
 #     # """Logout Form"""
 #     # session['logged_in'] = False
 #     return redirect(url_for('home'))
+
+@app.route("/upload", methods=['POST'])
+def upload():
+	## file upload ##
+    img = request.files['image']
+    
+    ## GridFs를 통해 파일을 분할하여 DB에 저장하게 된다
+    fs = gridfs.GridFS(db)
+    fs.put(img, filename = 'name')
+    
+    ## file find ##
+    data = client.grid_file.fs.files.find_one({'filename':'name'})
+    
+    ## file download ##
+    # my_id = data['_id']
+    # outputdata = fs.get(my_id).read()
+    # output = open('./images/'+'back.jpeg', 'wb')
+    # output.write(outputdata)
+    return jsonify({'msg':'저장에 성공했습니다.'})
 
 #################################
 ##  로그인을 위한 API            ##
